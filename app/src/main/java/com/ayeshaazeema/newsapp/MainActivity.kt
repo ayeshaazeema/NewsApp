db00eb38eb0d52b367516ceff12b8596527f4c57
ayeshaazeema <ayesha.azeema@gmail.com>
2021-02-09 10:44:23 +0700

    Initial commit

---
+++ app/src/main/java/com/ayeshaazeema/newsapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.ayeshaazeema.newsapp
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        fun getLaunchService(from: Context) = Intent(from, MainActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        ib_profile_main.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.ib_profile_main -> startActivity(Intent(ProfileActivity.getLaunchService(this)))
+        }
+    }
+}
