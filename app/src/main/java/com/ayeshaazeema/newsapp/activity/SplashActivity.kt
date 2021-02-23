d9f0a622cadaff92690abc356360ea5dc91b4c8c
ayeshaazeema <ayesha.azeema@gmail.com>
2021-02-23 12:20:03 +0700

    Fifth Commit

---
+++ app/src/main/java/com/ayeshaazeema/newsapp/activity/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.ayeshaazeema.newsapp.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.ayeshaazeema.newsapp.R
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT:Long = 4000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            startActivity(
+                Intent(this,
+                    SignInActivity::class.java)
+            )
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
